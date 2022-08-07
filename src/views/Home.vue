a44740c93cdde21396a2205d3628c985a1af4587
Veda-Ghadage <vedaghadage@gmail.com>
2022-08-07 11:56:37 +0530

    Project commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <adduser/>
+  </div>
+</template>
+
+<script>
+import adduser from '@/components/AddUser.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    adduser
+  }
+}
+</script>
