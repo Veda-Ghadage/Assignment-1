13f72305afae21ca58aaad9b01dce1ade63b1fda
Veda-Ghadage <vedaghadage@gmail.com>
2022-08-07 21:10:37 +0530

    add form

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>Add User
+  <div>
 <v-card
   class="mx-auto"
     max-width="500"
+    style="padding:20px"
 >
   <v-form
     ref="form"
@@ -18,9 +19,26 @@
     ></v-text-field>
 
     <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
+      v-model="age"
+      :counter="10"
+      :rules="nameRules"
+      label="Age"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="occupation"
+      :counter="10"
+      :rules="nameRules"
+      label="Occupation"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="address"
+      :counter="10"
+      :rules="nameRules"
+      label="Address"
       required
     ></v-text-field>
 
@@ -28,16 +46,16 @@
       v-model="select"
       :items="items"
       :rules="[v => !!v || 'Item is required']"
-      label="Item"
+      label="Gender"
       required
     ></v-select>
 
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
+    <v-text-field
+      v-model="mobileno"
+      :rules="nameRules"
+      label="Mobile Number"
       required
-    ></v-checkbox>
+    ></v-text-field>
 
     <v-btn
       :disabled="!valid"
@@ -73,28 +91,30 @@ export default {
   data: () => ({
     valid: true,
     name: '',
+    age: '',
+    occupation: '',
+    address: '',
+    mobileno: '',
+
     nameRules: [
       v => !!v || 'Name is required',
       v => (v && v.length <= 10) || 'Name must be less than 10 characters'
     ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-    ],
     select: null,
     items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4'
+      'Female',
+      'Male'
     ],
     checkbox: false
   }),
 
   methods: {
     validate () {
-      this.$refs.form.validate()
+      // this.$refs.form.validate()
+      // console.log(this.$refs.form.validate())
+      if (this.$refs.form.validate()) {
+        localStorage.setItem('name', this.name)
+      }
     },
     reset () {
       this.$refs.form.reset()
